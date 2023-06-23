b831cc3aa8a6496bceba371854b9d8707fb497c7
rk14283 <rk14283@my.bristol.ac.uk>
2023-06-23 13:39:56 +0100

    selecting parent element

--- cypress/e2e/mocha.cy.js
+++ cypress/e2e/mocha.cy.js
@@ -7,12 +7,12 @@ describe("Login feature test ", () => {
   it("it should visit the login page correctly on ", function () {
     //cy.get("css selector")
     cy.visit("cypress/index.html");
-    //my h1 found six elements, his found only 2
-    //HTML tag and id both
-    // cy.get("h1#header2");
-    // cy.get(".header1");
-    //we can have two classes and test passes
-    //both classes but don't put any space
-    cy.get(".main.header1");
+    //parent element class and child element
+    //cy.get(".course-list > .list1");
+    //Now we only get one bullet point
+    //cy.get(".course-list").children(".list1");
+
+    //children() command works in direct child and parent relationship
+    //grand parent
   });
 });
