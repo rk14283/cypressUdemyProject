eb606cf1eaa6e1da41e2911901d4c1d2840351be
rk14283 <rk14283@my.bristol.ac.uk>
2023-06-23 11:53:27 +0100

    testing with different sizes

--- cypress/e2e/mocha.cy.js
+++ cypress/e2e/mocha.cy.js
@@ -1,7 +1,12 @@
 /// <reference types = "Cypress"/>
 
 describe("Login feature test ", () => {
-  it("it should visit the login page correctly", function () {
-    cy.visit("/");
+  //An array to run tests on many different screens
+
+  ["macbook-15", "iphone-x"].forEach((size) => {
+    it("it should visit the login page correctly on " + size, function () {
+      //Allows to run tests against specific devices
+      cy.viewport(size);
+    });
   });
 });
